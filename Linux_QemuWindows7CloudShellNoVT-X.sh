bda5c5e5c6b0953577ed57b04829d5cad188c365
daibangcam <daibangcam@gmail.com>
2021-09-12 22:53:56 +0700

    Update Linux_QemuWindows7CloudShellNoVT-X.sh

--- Linux_QemuWindows7CloudShellNoVT-X.sh
+++ Linux_QemuWindows7CloudShellNoVT-X.sh
@@ -14,21 +14,5 @@ echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
-echo "Press F12, choose tab Console and paste these line then press Enter"
-echo ================================
-echo "function ClickConnect() {
-  console.log('Working')
-  document
-    .querySelector('#top-toolbar > colab-connect-button')
-    .shadowRoot.querySelector('#connect')
-    .click()
-} 
-setInterval(ClickConnect, 60000)"
-echo "================================"
-echo "Script by HanO"
-echo "Cell: +84978.39.41.43"
-echo "Email: daibangcam@gmail.com"
-echo "Website: hano.cf"
-echo "================================"
 cpu=$(echo nproc | bash)
 sudo qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=$cpu  -m 3072M -machine usb=on -device usb-tablet > /dev/null 2>&1
