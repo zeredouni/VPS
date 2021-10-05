19217c7f98985ef069070aafef7650cfc0d2ba42
zeredouni <80139822+zeredouni@users.noreply.github.com>
2021-10-05 17:12:03 +0000

    Update Linux_QemuWindows7ColabNoVT-X.sh

--- Linux_QemuWindows7ColabNoVT-X.sh
+++ Linux_QemuWindows7ColabNoVT-X.sh
@@ -1,7 +1,7 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-echo  "welcom wail ... "
-./ngrok authtoken 1z1SF3A9lNkzhtWJ55yy6YKCRfy_6k6Awr9mMY4Ts3qDn4ydr
+read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+./ngrok authtoken $CRP
 nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait for installing...
 echo "Installing QEMU (2-3m)..."
